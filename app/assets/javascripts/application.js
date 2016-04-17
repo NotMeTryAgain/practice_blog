defa76347f6a7957d2d3d57c591a601a188e104c
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-04-16 21:43:54 -0400

    google places API, returning cafe's in boston

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,44 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var map;
+var infowindow;
+
+function initMap() {
+  var boston = {lat: 42.3601, lng: -71.0589 };
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: boston,
+    zoom: 15
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: boston,
+    radius: 1000,
+    type: ['cafe']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
